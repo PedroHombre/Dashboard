f1581e22dc89f22df25738cb783f4668914332f8
PedroHombre <radekgornackiv2@gmail.com>
2021-08-12 20:41:42 +0200

    logout script

---
+++ controllers/logout-controller.php
@@ -0,0 +1,15 @@
+<?php 
+
+// INITIALIZE THE SESSION
+session_start();
+
+// UNSET ALL OF THE SESSION VARIABLES
+$_SESSION = array();
+
+// DESTROY THE SESSION
+session_destroy();
+
+// REDIRECT TO LOGIN PAGE
+header("location: /login.php");
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php require_once("controllers/session-check.php"); ?>
 
 <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+<a href="/controllers/logout-controller.php">Logout</a>
