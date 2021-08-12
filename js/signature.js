33ad7c65f81d1f8f625ee9501fcb983482ddde1f
PedroHombre <radekgornackiv2@gmail.com>
2021-08-12 21:27:00 +0200

    signature

---
+++ js/signature.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+    console.log("%cProject by %cPedroHombre %chttps://github.com/PedroHombre", "color: #0052ff; font-size: 2em;", "color: #0052ff; font-weight: 600; font-size: 2em;", "color: #0052ff; font-size: 2em;");
+}
--- templates/prelogin-footer.php
+++ templates/prelogin-footer.php
@@ -1,4 +1,5 @@
         <script src="https://unpkg.com/scrollreveal"></script>
         <script src="js/prelogin-main.js"></script>
+        <script src="js/signature.js"></script>
     </body>
 </html>
