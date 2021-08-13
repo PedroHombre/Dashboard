657781b85a352e57d3ec0412d306f88848e00f39
PedroHombre <radekgornackiv2@gmail.com>
2021-08-13 12:44:39 +0200

    slight changes

--- templates/prelogin-head.php
+++ templates/prelogin-head.php
@@ -5,8 +5,6 @@
         <meta name="charset" value="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <script src="js/signature.js"></script>
-
         <!-- STYLES -->
         <link rel="stylesheet" type="text/css" href="css/preloader.css">
         <link rel='stylesheet' type="text/css" href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css'>
