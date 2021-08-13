4068a1abbc4d686f94080ab94deeb8a323e3136a
PedroHombre <radekgornackiv2@gmail.com>
2021-08-13 12:28:44 +0200

    logo

--- js/prelogin-main.js
+++ js/prelogin-main.js
@@ -12,6 +12,40 @@ sr.reveal(`.wrapper, .title`, {
 
 // PARTICLES.JS
 particlesJS.load('particles-js', '/assets/prelogin/particles.json', function () {
-    particlesJS.color.value = "#ff0000"
     console.log('callback - particles.js config loaded');
 });
+
+// LOGO ANIMATIOB
+let logoCharacters = document.querySelectorAll(".title span:not(.dot)");
+for(let i = 0; i < logoCharacters.length; i++){
+    logoCharacters[i].addEventListener('mouseenter', ()=>{
+        // console.log(`Element przed: ${logoCharacters[i-1]} | Element hover: ${logoCharacters[i]} | Element po: ${logoCharacters[i+1]}`)
+        if(logoCharacters[i-1]){
+            logoCharacters[i-1].classList.add("active");
+        }
+        if (logoCharacters[i+1]) {
+            logoCharacters[i+1].classList.add("active");
+        }
+        if (logoCharacters[i - 2]) {
+            logoCharacters[i - 2].classList.add("active2");
+        }
+        if (logoCharacters[i + 2]) {
+            logoCharacters[i + 2].classList.add("active2");
+        }
+    })
+    logoCharacters[i].addEventListener('mouseleave', () => {
+        // console.log(`Element przed: ${logoCharacters[i-1]} | Element hover: ${logoCharacters[i]} | Element po: ${logoCharacters[i+1]}`)
+        if (logoCharacters[i-1]) {
+            logoCharacters[i-1].classList.remove("active");
+        }
+        if (logoCharacters[i+1]) {
+            logoCharacters[i+1].classList.remove("active");
+        }
+        if (logoCharacters[i - 2]) {
+            logoCharacters[i - 2].classList.remove("active2");
+        }
+        if (logoCharacters[i + 2]) {
+            logoCharacters[i + 2].classList.remove("active2");
+        }
+    })
+}
