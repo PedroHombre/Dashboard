aa375b1a3eccd28f8e245356e63d0825d19b0c17
PedroHombre <radekgornackiv2@gmail.com>
2021-08-13 13:20:19 +0200

    index template

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php require_once("controllers/session-check.php"); ?>
+<?php require_once("controllers/session-check.php"); 
+      require_once("templates/head.php"); ?>
 
 <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
 <a href="/controllers/controller-logout.php">Logout</a>
+
+<?php require_once("templates/footer.html"); ?>
---
+++ templates/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="charset" value="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- STYLES -->
+        <link rel="stylesheet" type="text/css" href="css/preloader.css">
+        <link rel='stylesheet' type="text/css" href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css'>
+        <link rel="stylesheet" type="text/css" href="css/prelogin.css">
+
+        <!-- AUTOMATIC TITLE -->
+        <title><?php echo $page_title ?> - Dashboard</title>
+    </head>
+    <body class="darkmode">
