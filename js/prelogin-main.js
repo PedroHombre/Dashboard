ac7c7b15803305e2fb0ddcf274c468579ba1cab3
PedroHombre <radekgornackiv2@gmail.com>
2021-08-13 13:25:13 +0200

    js tweaks

--- js/prelogin-main.js
+++ js/prelogin-main.js
@@ -1,4 +1,4 @@
-// SCROLL REVEAL
+// INITIALIZE SCROLL REVEAL SETTINGS
 const sr = ScrollReveal({
     origin: 'top',
     distance: '30px',
@@ -10,42 +10,42 @@ sr.reveal(`.wrapper, .title`, {
     interval: 200
 });
 
-// PARTICLES.JS
+// INITIALIZE PARTICLES.JS SETTINGS JSON FILE
 particlesJS.load('particles-js', '/assets/prelogin/particles.json', function () {
     console.log('callback - particles.js config loaded');
 });
 
-// LOGO ANIMATIOB
+// CHECK FOR HOVER ON LOGO CHARACTERS
 let logoCharacters = document.querySelectorAll(".title span:not(.dot)");
 for(let i = 0; i < logoCharacters.length; i++){
+    // ADD CLASS ON MOUSE ENTER
     logoCharacters[i].addEventListener('mouseenter', ()=>{
-        // console.log(`Element przed: ${logoCharacters[i-1]} | Element hover: ${logoCharacters[i]} | Element po: ${logoCharacters[i+1]}`)
         if(logoCharacters[i-1]){
             logoCharacters[i-1].classList.add("active");
         }
         if (logoCharacters[i+1]) {
             logoCharacters[i+1].classList.add("active");
         }
-        if (logoCharacters[i - 2]) {
-            logoCharacters[i - 2].classList.add("active2");
+        if (logoCharacters[i-2]) {
+            logoCharacters[i-2].classList.add("active2");
         }
-        if (logoCharacters[i + 2]) {
-            logoCharacters[i + 2].classList.add("active2");
+        if (logoCharacters[i+2]) {
+            logoCharacters[i+2].classList.add("active2");
         }
     })
+    // REMOVE CLASS ON MOUSE LEAVE
     logoCharacters[i].addEventListener('mouseleave', () => {
-        // console.log(`Element przed: ${logoCharacters[i-1]} | Element hover: ${logoCharacters[i]} | Element po: ${logoCharacters[i+1]}`)
         if (logoCharacters[i-1]) {
             logoCharacters[i-1].classList.remove("active");
         }
         if (logoCharacters[i+1]) {
             logoCharacters[i+1].classList.remove("active");
         }
-        if (logoCharacters[i - 2]) {
-            logoCharacters[i - 2].classList.remove("active2");
+        if (logoCharacters[i-2]) {
+            logoCharacters[i-2].classList.remove("active2");
         }
-        if (logoCharacters[i + 2]) {
-            logoCharacters[i + 2].classList.remove("active2");
+        if (logoCharacters[i+2]) {
+            logoCharacters[i+2].classList.remove("active2");
         }
     })
 }
