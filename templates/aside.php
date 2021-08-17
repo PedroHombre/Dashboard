99e63808c452fdfac16789e6a1b0e2a3d069ebcc
PedroHombre <radekgornackiv2@gmail.com>
2021-08-17 15:12:34 +0200

    aside

--- templates/aside.php
+++ templates/aside.php
@@ -3,5 +3,22 @@
         <div class="aside-logo-wrapper">
             <?php require_once("templates/logo.html"); ?>
         </div>
+        <div class="links-wrapper">
+            <a href="#">
+                <i class='bx bx-cog'></i>
+                Settings
+            </a>
+            <?php
+            if($_SESSION["accountType"]):
+                $accountType = $_SESSION["accountType"];
+                if($accountType == "admin"): ?>
+                    <a href="/admin/admin.php">
+                        <i class='bx bxs-label' ></i>
+                        Admin panel
+                    </a>
+                <?php endif;
+            endif;
+        ?>
+        </div>
     </aside>
 </div>
