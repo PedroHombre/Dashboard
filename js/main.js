23edd67b9c4f6e72c6de6d7764353a12d55be641
PedroHombre <radekgornackiv2@gmail.com>
2021-08-18 18:51:06 +0200

    improved animations

--- js/main.js
+++ js/main.js
@@ -1,34 +1,11 @@
-// CHECK FOR HOVER ON LOGO CHARACTERS
-let logoCharacters = document.querySelectorAll(".title span:not(.dot)");
-for (let i = 0; i < logoCharacters.length; i++) {
-    // ADD CLASS ON MOUSE ENTER
-    logoCharacters[i].addEventListener('mouseenter', () => {
-        if (logoCharacters[i - 1]) {
-            logoCharacters[i - 1].classList.add("active");
-        }
-        if (logoCharacters[i + 1]) {
-            logoCharacters[i + 1].classList.add("active");
-        }
-        if (logoCharacters[i - 2]) {
-            logoCharacters[i - 2].classList.add("active2");
-        }
-        if (logoCharacters[i + 2]) {
-            logoCharacters[i + 2].classList.add("active2");
-        }
-    })
-    // REMOVE CLASS ON MOUSE LEAVE
-    logoCharacters[i].addEventListener('mouseleave', () => {
-        if (logoCharacters[i - 1]) {
-            logoCharacters[i - 1].classList.remove("active");
-        }
-        if (logoCharacters[i + 1]) {
-            logoCharacters[i + 1].classList.remove("active");
-        }
-        if (logoCharacters[i - 2]) {
-            logoCharacters[i - 2].classList.remove("active2");
-        }
-        if (logoCharacters[i + 2]) {
-            logoCharacters[i + 2].classList.remove("active2");
-        }
-    })
-}
+// INITIALIZE SCROLL REVEAL SETTINGS
+const sr = ScrollReveal({
+    origin: 'left',
+    distance: '30px',
+    duration: 2000,
+    reset: false
+});
+
+sr.reveal(`.links-wrapper a`, {
+    interval: 200
+});
--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+
+        <!-- META TAGS -->
         <meta name="charset" value="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -8,6 +10,9 @@
         <link rel="stylesheet" type="text/css" href="css/preloader.css">
         <link rel='stylesheet' type="text/css" href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css'>
         <link rel="stylesheet" type="text/css" href="css/main.css">
+
+        <!-- SCRIPTS -->
+        <script src="https://unpkg.com/scrollreveal"></script>
 
         <!-- AUTOMATIC TITLE -->
         <title><?php echo $page_title ?> - Dashboard</title>
