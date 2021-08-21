7e6dc8945ff712065f09d402ea839380b47a5ce4
PedroHombre <radekgornackiv2@gmail.com>
2021-08-21 14:01:31 +0200

    aside titles

--- templates/aside.php
+++ templates/aside.php
@@ -6,20 +6,20 @@
             </a>
         </div>
         <div class="links-wrapper">
-            <a href="/index.php" <?php if($pageName == "Home"){ ?>class="active"<?php } ?>>
+            <a href="/index.php" <?php if($pageName == "Home"){ ?>class="active"<?php } ?> title="Homepage">
                 <i class='bx bxs-home'></i>
             </a>
-            <a href="/my-account.php">
+            <a href="/my-account.php" title="My account">
                 <i class='bx bxs-user'></i>
             </a>
-            <a href="#">
+            <a href="#" title="Settings">
                 <i class='bx bx-cog'></i>
             </a>
             <?php
                 if($_SESSION["accountType"]):
                     $accountType = $_SESSION["accountType"];
                     if($accountType == "admin"): ?>
-                        <a href="/admin/admin.php">
+                        <a href="/admin/admin.php" title="Admin panel">
                             <!-- <i class='bx bxs-user-account' ></i> -->
                             <i class='bx bxs-crown' ></i>
                         </a>
@@ -28,7 +28,7 @@
             ?>
         </div>
         <div class="logout-btn">
-            <a href="/controllers/controller-logout.php">
+            <a href="/controllers/controller-logout.php" title="Logout">
                 <!-- <i class='bx bx-log-out' ></i> -->
                 <i class='bx bx-power-off' ></i>
             </a>
