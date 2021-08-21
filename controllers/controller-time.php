b2332fc615a24cea600abcdace5f5303ea41c0cb
PedroHombre <radekgornackiv2@gmail.com>
2021-08-21 14:04:22 +0200

    date controller

--- controllers/controller-time.php
+++ controllers/controller-time.php
@@ -1,8 +1,13 @@
 <?php
 
+// GET CURRENT DATE AND TIME
 $currentTime = date_create();
-
+$year = $currentTime->format('y');
+$month = $currentTime->format('m');
+$day = $currentTime->format('d');
 $hour = $currentTime->format('H');
+$minute = $currentTime->format('m');
+$second = $currentTime->format('s');
 
 
 if( $hour >= 5 && $hour < 12 ) {
