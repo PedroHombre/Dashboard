919df682c6129167fb5c3e6b1711b70d98f0bdcd
PedroHombre <radekgornackiv2@gmail.com>
2021-08-24 22:44:53 +0200

    session debugging

--- controllers/controller-index.php
+++ controllers/controller-index.php
@@ -5,4 +5,11 @@ $page_title = "Welcome " . ($_SESSION["username"]) . " - Dashboard";
 // PAGE NAME
 $pageName = "Home";
 
+$loginCount = $_SESSION["loginCount"];
+echo $loginCount;
+echo $_SESSION["loginCount"];
+if(!isset($_SESSION["loginCount"])) {
+    echo "nima";
+}
+
 ?>
